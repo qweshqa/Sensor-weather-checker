2c5a6695858b6d14089c862fc9cec6f53a273cb7
qweshqa <143135070+qweshqa@users.noreply.github.com>
2024-02-27 03:17:57 +0200

    Update RestApiProjectApplication.java

--- src/main/java/com/example/restapiproject/RestApiProjectApplication.java
+++ src/main/java/com/example/restapiproject/RestApiProjectApplication.java
@@ -1,26 +1,9 @@
 package com.example.restapiproject;
 
-import com.example.restapiproject.dto.MeasurementDTO;
-import com.example.restapiproject.dto.SensorDTO;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 
 @SpringBootApplication
 public class RestApiProjectApplication {
